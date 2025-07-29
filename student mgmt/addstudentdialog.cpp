77ade9e4b9525bb0b619397d811c603ee1761232
Prashasti <paulprashasti@gmail.com>
2025-07-30 01:03:16 +0530

    Update addstudentdialog.cpp

--- student mgmt/addstudentdialog.cpp
+++ student mgmt/addstudentdialog.cpp
@@ -1,5 +1,5 @@
-#include "addstudentdialog.h"             //includes header file for this class.
-#include "ui_addstudentdialog.h"          //Includes the UI class generated from the .ui file
+#include "addstudentdialog.h"             
+#include "ui_addstudentdialog.h"          
 #include <QMessageBox>
 
 
@@ -14,24 +14,24 @@ AddStudentDialog::~AddStudentDialog()
 {
     delete ui;
 }
-Student AddStudentDialog::getStudent() const {   //Student means func will return complete Student object.
+Student AddStudentDialog::getStudent() const {   
     Student s;                              // Create a local Student object 's'.
     s.name=ui->namelineEdit->text();        // gets name from the text input field.
     s.rollNo=ui->rolllineEdit->text();
     s.branch=ui->branchlineEdit->text();
     s.gender=ui->gendercomboBox->currentText();
     s.address=ui->addresslineEdit->text();
-    return s;                                   //Returns fully filled Student object.
+    return s;                                  
 
 }
 
 void AddStudentDialog::on_btnSaveStudent_clicked()
 {
-    if(ui->namelineEdit->text().isEmpty()){             // Check if name field is empty.
+    if(ui->namelineEdit->text().isEmpty()){          
 
         QMessageBox::warning(this,"Input Error","Name cannot be empty.");
         return;
     }
-    accept();                     //accept - closes the dialog and indicate success.
+    accept();                    
 }
 
