c8dd41f7e680bffadd6da57cecd80ef4b3f39de6
Prashasti <paulprashasti@gmail.com>
2025-07-30 01:12:24 +0530

    Update main.cpp

--- student mgmt/main.cpp
+++ student mgmt/main.cpp
@@ -1,16 +1,16 @@
-#include "loginwindow.h"      //custom class handles login dialog (inherits from QDialog).
-#include "mainwindow.h"       //custom class for main application window (inherits from QMainWindow)
-#include <QApplication>       // core application class required for all Qt GUI applications.
+#include "loginwindow.h"      
+#include "mainwindow.h"     
+#include <QApplication>       
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);  //Initializes the Qt application.
+    QApplication a(argc, argv); 
 
-    LoginWindow login;                      //Creates instance of login dialog.
-    if(login.exec()==QDialog::Accepted){    //if login accepted
-       MainWindow w;                         //create main window
-        w.show();                           //show main window
-        return a.exec();                    //Start Qt event loop to keep application running
+    LoginWindow login;                     
+    if(login.exec()==QDialog::Accepted){   
+       MainWindow w;                         
+        w.show();                           
+        return a.exec();                    
     }
-    return 0;                             //If login dialog was cancel/login failed, app exits immediately
+    return 0;                           
 }
