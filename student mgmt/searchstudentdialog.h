a89252f96fa33cdc1dbff4d3339a083393b800bf
Prashasti <paulprashasti@gmail.com>
2026-01-21 23:39:13 +0530

    Update searchstudentdialog.h

--- student mgmt/searchstudentdialog.h
+++ student mgmt/searchstudentdialog.h
@@ -1,8 +1,8 @@
 #ifndef SEARCHSTUDENTDIALOG_H
 #define SEARCHSTUDENTDIALOG_H
 
-#include <QDialog>                //class will inherit from it, making it a dialog box
-#include<QVector>                 //Qt container used to hold a list of Student
+#include <QDialog>                
+#include<QVector>                
 #include"student.h"
 
 
@@ -10,11 +10,11 @@ namespace Ui {
 class searchstudentDialog;
 }
 
-class searchstudentDialog : public QDialog   //searchstudentDialog, which inherits from QDialog.
+class searchstudentDialog : public QDialog   
 {
     Q_OBJECT
 
-public:                       //Constructor: Takes a reference to a list of Student objects (students) and parent widget.
+public:                       
     explicit searchstudentDialog( const QVector<Student> &students, QWidget *parent = nullptr);
     ~searchstudentDialog();
 
@@ -32,12 +32,3 @@ private:
 #endif // SEARCHSTUDENTDIALOG_H
 
 
-  // const QVector<Student> &students---> is a book on which can not write but  only read
-// whereas studentList is a photocopy on ehich u can write
-//studentList(students): you're explicitly copying that list into your class's
-
-
-  //QVector<Student>--> A Qt container (like a dynamic array or list) that holds Student objects
-//&--->	A reference, meaning you're referring to an existing vector, not making a copy
-// const--->	You promise not to modify the vector or its contents
-// students--->	The name of the variable (
