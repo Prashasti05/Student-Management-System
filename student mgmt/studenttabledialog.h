237bb30d5c15514cabb135f42d049578f1891404
Prashasti <paulprashasti@gmail.com>
2026-01-22 00:39:31 +0530

    Update studenttabledialog.h

--- student mgmt/studenttabledialog.h
+++ student mgmt/studenttabledialog.h
@@ -1,8 +1,8 @@
-#ifndef STUDENTTABLEDIALOG_H    //ifndef
-#define STUDENTTABLEDIALOG_H   //define header file
+#ifndef STUDENTTABLEDIALOG_H    
+#define STUDENTTABLEDIALOG_H   
 
 #include <QDialog>
-#include"student.h"    // Include the Student class definition
+#include"student.h"    
 
 #include<QVector>    //Include QVector container class to handle a list of students
 
@@ -10,7 +10,7 @@ namespace Ui {
 class StudentTableDialog;
 }
 
-class StudentTableDialog : public QDialog          // Declare StudentTableDialog class, inherited from QDialog
+class StudentTableDialog : public QDialog          
 {
     Q_OBJECT
 
@@ -18,11 +18,8 @@ public:
     explicit StudentTableDialog(const QVector<Student> &students, QWidget *parent = nullptr);
     ~StudentTableDialog();
 
-    /*When you show a dialog using .show() or .exec(), Qt automatically calls showEvent() at last moment/just before the window appears.
-     By overriding this function, you can run your own code (like loading/refreshing data) right before the dialog is shown.*/
 
     void showEvent(QShowEvent *event) override;
-    // Overrides the showEvent() method to perform actions when the dialog is shown (e.g., load data)
 
 private:
     Ui::StudentTableDialog *ui;
